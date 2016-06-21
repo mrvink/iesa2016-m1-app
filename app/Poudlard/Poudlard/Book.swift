13a9d88a2f09e007b209d560b6d87c77b9fe87d5
mrvink <vinkdesign@gmail.com>
2016-06-21 14:44:18 +0200

    #2 Add news classes Book and BookStore

---
+++ app/Poudlard/Poudlard/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  Poudlard
+//
+//  Created by Vincent Picard on 21/06/2016.
+//  Copyright © 2016 Vink. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var description: String = ""
+}
---
+++ app/Poudlard/Poudlard/BookStore.swift
@@ -0,0 +1,13 @@
+//
+//  BookStore.swift
+//  Poudlard
+//
+//  Created by Vincent Picard on 21/06/2016.
+//  Copyright © 2016 Vink. All rights reserved.
+//
+
+import Foundation
+
+class BookStore {
+    
+}
