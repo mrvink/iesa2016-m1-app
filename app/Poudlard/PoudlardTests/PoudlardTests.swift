2791b6648fc034df53412198c2c998df1c825e90
mrvink <vinkdesign@gmail.com>
2016-06-21 12:48:28 +0200

    #1 application icons

---
+++ app/Poudlard/PoudlardTests/PoudlardTests.swift
@@ -0,0 +1,36 @@
+//
+//  PoudlardTests.swift
+//  PoudlardTests
+//
+//  Created by Vincent Picard on 21/06/2016.
+//  Copyright © 2016 Vink. All rights reserved.
+//
+
+import XCTest
+@testable import Poudlard
+
+class PoudlardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
